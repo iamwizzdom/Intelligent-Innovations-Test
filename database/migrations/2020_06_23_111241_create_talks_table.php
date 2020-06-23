72981899a97b1fb616bab37dc389b077ae530ee5
iamwizzdom <wisdomemenike70@gmail.com>
2020-06-23 16:42:37 +0100

    Reduced talk uuid length

--- database/migrations/2020_06_23_111241_create_talks_table.php
+++ database/migrations/2020_06_23_111241_create_talks_table.php
@@ -15,7 +15,7 @@ class CreateTalksTable extends Migration
     {
         Schema::create('talks', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
+            $table->string('uuid', 200)->unique();
             $table->string('topic');
             $table->string('description')->nullable();
             $table->date('date');
